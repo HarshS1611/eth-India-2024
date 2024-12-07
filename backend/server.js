2363956311c190da578275ebcef1b21a6db4b655
sahas-01 <sahasvivek001@gmail.com>
2024-12-07 22:03:13 +0530

    feat : backend using akave

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const routes = require("./routes");
+
+const app = express();
+const port = 8001;
+
+app.use(express.json());
+app.use("/", routes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
